e55c4b9d46e8eea9d88edbe985f8f8e0b5071e8c
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-11-01 00:19:03 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1626A | Equidistant Letters/146646855.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       string str;
+       cin>>str;
+       
+       sort(str.begin() , str.end());
+       
+       cout<<str<<endl;
+        
+    }
+}
