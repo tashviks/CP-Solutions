0a7ad5e30f7f66883058f60a93ddd41326053ff3
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-11-01 00:14:03 +0530

    Committing files

---
+++ CodeForces/GNU C++17/459A | Pashmak and Garden/161870137.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h> 
+using namespace std;
+// The code is written by Tashvik Srivastava  // 
+#define GODSPEED ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define TEST int t; cin>>t; while(t--)
+#define NoTEST int t = 1; while(t--)
+#define inp(v) for (int i = 0; i < n; ++i) {cin>>temp;v.push_back(temp);}
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define pb push_back
+#define popb pop_back
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+ 
+//first try (time taken about 15 min)
+ 
+void solve()
+{
+ ll x1 , y1 , x2 , y2;
+ cin>>x1>>y1>>x2>>y2;
+ if (x1 != x2 && y1 != y2 && abs(x1 - x2) != abs(y1 - y2))
+        cout << -1 << endl;
+    else if (x1 == x2)
+        cout << x1 + abs(y1 - y2) << " " << y1 << " " << x2 + abs(y1 - y2) << " " << y2 << endl;
+    else if (y1 == y2)
+        cout << x1 << " " << y1 + abs(x1 - x2) << " " << x2 << " " << y2 + abs(x1 - x2) << endl;
+    else
+        cout << x1 << " " << y2 << " " << x2 << " " << y1 << endl;
+   
+    return;
+    
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+int main() 
+{
+ GODSPEED
+ 
+    NoTEST
+    {
+        solve();
+    }
+}
+ 
