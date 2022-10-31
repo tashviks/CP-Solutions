75e55fdc5951724b7868393264dfcce0fc2c0938
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-11-01 00:09:14 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1705A | Mark the Photographer/164350009.cpp
@@ -0,0 +1,185 @@
+ 
+//interpretation problem first three time
+ 
+ 
+#include <bits/stdc++.h> 
+#include<math.h>
+using namespace std;
+//------------------ The code is written by Tashvik Srivastava ------------------// 
+#define GODSPEED ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define TEST int t; cin>>t; for(int i = 0;i<t;i++)
+#define NoTEST int t = 1; while(t--)
+#define inp(v) for (int i = 0; i < n; ++i) {cin>>temp;v.push_back(temp);}
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define pb push_back
+#define popb pop_back
+#define forn(i, x, n) for (ll i = x; i < n; i++)
+#define fornb(i, n, x) for (ll i = n; i >=x; i--)
+const int  MAX = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+ 
+int remove0(int n);
+int ispalin(string S);
+//first try (time taken about 15 min)
+//best case complexity of O(n)
+ 
+ 
+//------------------------------------------------------------------------------------------------------------------
+//to find if given sum is sum of pairs in an array      
+int FindPairSum(std::vector<int> A, int N, int SUM)
+{
+    int i = 0;int j = N - 1;
+    while (i < j) {if (A[i] + A[j] == SUM)      return 1;
+                   else if (A[i] + A[j] < SUM)  i++;
+                   else                         j--;
+                  }
+                   return 0;                    
+}
+//-------------------------------------------------------------------------------------------------------------------
+//to find sum of all vectors in an array
+int vecsum(std::vector<int> v , int n)
+{
+    int sum = 0;
+    forn(i,0,n)
+    {
+        sum+=v[i];
+    }
+    return sum;
+}
+//------------------------------------------------------------------------------------------------------------------
+//prefix sum vector
+vector<int> prefixsum(std::vector<int> v , int n)
+{
+    std::vector<int> r;
+    r[0]=v[0];
+    for (int i = 1; i < n; ++i)
+    {
+        /* code */
+        r[i]=r[i-1]+v[i];
+ 
+    }
+    return r;
+}
+//------------------------------------------------------------------------------------------------------------------
+//returns gcd
+long long gcd(long long int a, long long int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+//------------------------------------------------------------------------------------------------------------------
+//returns lcm
+long long lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+//------------------------------------------------------------------------------------------------------------------
+ll isSorted(vector<ll> A)
+{
+ 
+    ll n = A.size();
+    forn(i, 0, n - 1)
+    {
+        if (A[i] > A[i + 1])
+            return 0;
+    }
+    return 1;
+}
+//------------------------------------------------------------------------------------------------------------------
+long long int printPair(std::vector<ll> arr, ll n, ll sum) 
+{ 
+    ll count = 0; // Initialize result 
+  
+    // Consider all possible pairs and check 
+    // their sums 
+    bool *visited=new bool[n]();
+    for (ll i = 0; i < n; i++){ 
+         if(visited[i]) continue;
+        for (ll j = i + 1; j < n; j++){ 
+               if(visited[j]) continue;
+            if (arr[i] + arr[j] == sum){
+                   count++;
+                   visited[j]=true;
+                   break;
+                } 
+         }     
+   }       
+ 
+     return count;           
+} 
+//------------------------------------------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------------------------------------------
+//solve function
+void solve()
+{
+     int n , x;
+     cin>>n>>x;
+            std::vector<int> v(2*n);
+ 
+            for(auto &it : v)
+                cin>>it;
+ 
+            sort(v.begin(), v.end());
+ 
+           int mid = n-1 , last = 2*n -1 ;
+ 
+            for(int i = 0;i<n;i++)
+            {
+                if(v[last]-v[mid]>=x)
+                {
+                    mid--;
+                    last--;
+                }
+ 
+                else
+                {
+                    cout<<"NO"<<endl;
+                    return;
+                }
+            }
+ 
+            cout<<"YES"<<endl;
+ 
+ 
+  
+ 
+    }
+ 
+    
+ 
+ 
+ 
+//------------------------------------------------------------------------------------------------------------------  
+int main() 
+{
+    GODSPEED
+ 
+    TEST
+    {
+        solve();
+    }
+}
+ 
+ 
+    
+        
+        
