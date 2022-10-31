d72c211607c351db30e6951af282876bdb285166
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-10-31 23:18:10 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1749D | Counting Arrays/177356982.cpp
@@ -0,0 +1,435 @@
+#include <bits/stdc++.h> 
+#include <math.h>
+#include<stdint.h>
+#include<cstdint>
+#include <algorithm>
+using namespace std;
+//------------------ The code has been written by Tashvik Srivastava ------------------// 
+#define GODSPEED ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define TEST int t; cin>>t; for(int i = 0;i<t;i++)  
+#define NoTEST int t = 1; while(t--)
+#define inp(v) for (int i = 0; i < n; ++i) {cin>>tp;v.push_back(tp);}
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define pb push_back
+#define popb pop_back
+#define forn(i, x, n) for (ll i = x; i < n; i++)
+#define fornb(i, n, x) for (ll i = n; i >=x; i--)
+#define endl "\n" 
+#define unt uint32_t
+#define bins(x , y , z) binary_search(x , y , z)
+#define c(x) __builtin_ctz(x)
+const int  MAX = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+int t,n,m,q,u,v,T,a,i[200200];
+//______________________________________________________________________________________________
+ 
+//____________________________________________________________________________________________
+int remove0(int n);
+int ispalin(string S);
+//first try (time taken about 15 min)
+//best case complexity of O(n)
+int findd(std::vector<int> v , int  n , int x);
+//------------------------------------------------------------------------------------------------------------------
+//to find if given sum is sum of pairs in an array      
+int FindPairSum(std::vector<int> A, int N, int SUM)
+{
+    int i = 0;int j = N - 1;
+    while (i < j) {if (A[i] + A[j] == SUM)      return 1;
+                   else if (A[i] + A[j] < SUM)  i++;
+                   else                         j--;
+                  }
+                   return 0;                    
+}
+//-------------------------------------------------------------------------------------------------------------------
+//to find sum of all vectors in an array
+int vecsum(std::vector<int> v , int n)
+{
+    int sum = 0;
+    forn(i,0,n)
+    {
+        sum+=v[i];
+    }
+    return sum;
+}
+//------------------------------------------------------------------------------------------------------------------
+//tpix sum vector
+vector<int> tpixsum(std::vector<int> v , int n)
+{
+    std::vector<int> r;
+    r[0]=v[0];
+    for (int i = 1; i < n; ++i)
+    {
+        /* code */
+        r[i]=r[i-1]+v[i];
+    }
+    return r;
+}
+//------------------------------------------------------------------------------------------------------------------
+//returns gcd
+ll gcd(ll a, ll b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+//------------------------------------------------------------------------------------------------------------------
+//returns lcm
+long long lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+//------------------------------------------------------------------------------------------------------------------
+ll isSorted(vector<ll> A)
+{
+ 
+    ll n = A.size();
+    forn(i, 0, n - 1)
+    {
+        if (A[i] > A[i + 1])
+            return 0;
+    }
+    return 1;
+}
+//------------------------------------------------------------------------------------------------------------------
+long long int printPair(std::vector<ll> arr, ll n, ll sum) 
+{ 
+    ll cnt = 0; // Initialize result 
+  
+    // Consider all possible pairs and check 
+    // their sums 
+    bool *visited=new bool[n]();
+    for (ll i = 0; i < n; i++){ 
+         if(visited[i]) continue;
+        for (ll j = i + 1; j < n; j++){ 
+               if(visited[j]) continue;
+            if (arr[i] + arr[j] == sum){
+                   cnt++;
+                   visited[j]=true;
+                   break;
+                } 
+         }     
+   }       
+ 
+     return cnt;           
+} 
+//------------------------------------------------------------------------------------------------------------------
+set<long long> prim(int n)
+{
+    n++;
+    bool *arr=new bool[n];
+    for(int i=0;i<n;i++)
+        arr[i]=true;
+    set<long long> se;
+    for(int i=2;i*i<n;i++)
+        if(arr[i])
+            for(int k=i*i;k<n;k+=i)
+                arr[k]=false;
+    for(int i=2;i<n;i++)
+        if(arr[i])
+            se.insert(i);
+    return se;
+}
+//------------------------------------------------------------------------------------------------------------------
+bool check_eqvec(std::vector<int> arr, int n)
+{   
+    bool flag = false;
+ 
+    for(int i = 0; i < n - 1; i++)      
+    {         
+        if(arr[i] != arr[i + 1])
+            flag = true;
+    }
+ 
+    return flag;
+}
+//------------------------------------------------------------------------------------------------------------------
+void remove(std::vector<ll> v)
+{
+    auto end = v.end();
+    for (auto it = v.begin(); it != end; ++it) {
+        end = std::remove(it + 1, end, *it);
+    }
+ 
+    v.erase(end, v.end());
+}
+//------------------------------------------------------------------------------------------------------------------
+//RETURNS A SET OF DIVISORS OF N
+set<int64_t> printDivisors(int64_t n)
+{
+    // Vector to store half of the divisors
+    set<int64_t> v;
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+ 
+            // check if divisors are equal
+            if (n / i == i)
+            {
+                // printf("%d ", i);
+                v.insert(i);
+            }
+            else
+            {
+                // printf("%d ", i);
+                v.insert(i);
+                // push the second divisor in the vector
+                v.insert(n / i);
+            }
+        }
+    }
+    return v;
+    // // The vector will be printed in reverse
+    // for (int i = v.size() - 1; i >= 0; i--)
+    //     printf("%d ", v[i]);
+}
+//------------------------------------------------------------------------------------------------------------------
+ 
+//------------------------------------------------------------------------------------------------------------------
+struct hashFunction
+{
+  size_t operator()(const pair<int , 
+                    int> &x) const
+  {
+    return x.first ^ x.second;
+  }
+};
+ 
+ 
+//------------------------------------------------------------------------------------------------------------------
+ 
+   int cntdig(int n)
+   {
+    int cnt = 0 ;
+ 
+    while(n!=0)
+    {
+        n/=10;
+        cnt++;
+    }
+ 
+    return cnt;
+   }
+ 
+//------------------------------------------------------------------------------------------------------------------
+bool isPowerOfTwo(int n)
+{
+   if(n==0)
+   return false;
+ 
+   return (ceil(log2(n)) == floor(log2(n)));
+}
+//-------------------------------------------------------------------------------------
+int Search(int arr[], int l, int r, int x)
+{
+    if (r >= l) 
+    {
+       int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return Search(arr, l, mid - 1, x);
+        return Search(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+//-------------------------------------------------------------------------------------
+ll sum_nat(int n)
+{
+    return n * 1ll * (n + 1) / 2;
+}
+//-------------------------------------------------------------------------------------
+bool check_sqr(long double x)
+{
+    if(x>=0)
+    {
+    ll sr = sqrt(x);
+    return(sr*sr==x);
+    }
+    return false;
+ 
+}
+//-------------------------------------------------------------------------------------
+bool check_even(int n)
+{
+    if(n & 1)
+        return 0;
+    else 
+        return 1;
+}
+//-------------------------------------------------------------------------------------
+int count1bits(uint32_t n) 
+{                               // counts number of ones in a bitmask
+   return bitset < 32 > (n).count();
+}
+//-------------------------------------------------------------------------------------
+double getroot(int n , int m )
+{
+    /* 
+ 
+     // Using Binary Search
+ 
+    double low = 1 , high = m, eps = 1e-6;
+ 
+    while((high - low)>eps)
+    {
+        double mid = (low+high)/2;
+        if(pow(mid , n)<m) low = mid;
+        else high = mid;
+    }
+ 
+    */
+    return pow(m , (double)(1.0/(double)n));
+ 
+}
+//-------------------------------------------------------------------------------------
+ int factorial( int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+//-------------------------------------------------------------------------------------
+int f(int x){
+    cin>>u>>v;
+    u=min(u,v);
+    a+=!(i[u]+=x)-(i[u]==1&x==1);
+}
+ 
+//-------------------------------------------------------------------------------------
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+ 
+//-------------------------------------------------------------------------------------
+//solve function
+bool helper(deque<int> &a,int k)
+{
+    int i=1;
+    while(i<=k)
+    {
+        sort(a.begin(),a.end());
+        if(a[0]>k-i+1)
+        {
+            return false;
+        }
+        int ind=a.size();
+        for(int j=a.size()-1;j>=0;--j)
+        {
+            if(a[j]<=k-i+1)
+            {
+                ind=j;
+                break;
+            }
+        }
+        a.erase(a.begin()+ind);
+        a[0]+=k-i+1;
+        i++;
+    }
+    return true;
+}
+ 
+void solve()
+{
+   
+ 
+ /*int n;
+ cin>>n;
+ 
+ multiset<int> a;
+ 
+ forn(i , 0 , n)
+ {
+    int x;
+    cin>>x;
+    a.insert(x);
+ }
+ 
+ if(n==1)
+ {
+    if(*a.begin()<=1)
+        cout<<"1\n";
+    else
+        cout<<"0\n";
+    continue;
+ }
+ 
+ int lo = 0 , ji = n;
+ int ans = 0;
+ 
+ while(lo<=hi)
+ {
+    multiset<int> b = a;
+    int mid = (hi - lo)/2 + lo;
+    int fl = 1;
+    forn(i , 1 , mid+1)
+    {
+        auto d = b.upper_bound(mid-i+1);
+        if(d==b.begin())
+        {
+            fl = 0;
+            break;
+        }
+        else
+        {
+            d--;
+            b.erase(b.find(*d));
+            int p = *b.begin();
+            b.erase(b.find(p));
+            b.inser(p+mid-i+1)
+        }
+    }
+ 
+    if(fl)
+        ans = mid , lo = mid+1;
+    else
+        hi = mid-1;
+}
+cout<<ans<<endl;
+*/
+const int P=998244353;
+ll n,m,ans=0,mul=1,nm=1,tmp=1;
+    cin>>n>>m;
+    for(ll i=1;i<=n;i++)
+    {
+        if(mul<=m&&__gcd(mul,i)==1)
+            mul*=i;
+        nm=m%P*nm%P;
+        tmp=(m/mul)%P*tmp%P;
+        ans=(ans+nm-tmp+P)%P;
+    }
+    cout<<ans<<endl;
+ 
+ }
+ 
+ 
+ 
+   
+//------------------------------------------------------------------------------------------------------------------  
+int main()
+{
+    GODSPEED
+    NoTEST
+    solve();
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
