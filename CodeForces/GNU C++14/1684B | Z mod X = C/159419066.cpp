6fccf54c7c6f5e47f3efec69032751e36e11135b
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-11-01 00:18:05 +0530

    Committing files

---
+++ CodeForces/GNU C++14/1684B | Z mod X = C/159419066.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+using namespace std;
+// The code is written by Tashvik Srivastava  // 
+#define GODSPEED ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define TEST int t; cin>>t; while(t--)
+#define loop_array for(int i =0;i<(sizeof(arr)/sizeof(arr[0]));i++)
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define pb push_back
+#define popb pop_back
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+ 
+ 
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << " " << b + c << " " << c << "\n";
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
