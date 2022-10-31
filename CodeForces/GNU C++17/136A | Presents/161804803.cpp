330387f1321234572da45f0fd2bbe969dae1b1ba
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-11-01 00:14:06 +0530

    Committing files

---
+++ CodeForces/GNU C++17/136A | Presents/161804803.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h> 
+using namespace std;
+// The code is written by Tashvik Srivastava  // 
+#define GODSPEED ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define TEST int t; cin>>t; while(t--)
+#define NoTEST int t = 1; while(t--)
+#define inp(v) for (int i = 0; i < n; ++i) {cin>>temp;v.push_back(temp);}
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define pb push_back
+#define popb pop_back
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+ 
+ 
+//first try (time taken about 15 min)
+ 
+void solve()
+{
+ int n;
+    cin>>n;
+    int k;
+    std::vector<int> a(n);
+    for(int i=1; i<=n; i++)
+    {
+        cin>>k;
+        a[k]=i;
+    }
+    for(int i=1; i<=n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return ;
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+int main() 
+{
+ GODSPEED
+ 
+    NoTEST
+    {
+        solve();
+    }
+}
+ 
