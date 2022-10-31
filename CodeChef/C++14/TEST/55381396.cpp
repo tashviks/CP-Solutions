0af4da97c941d76d7e324e7ad97dcff83223590d
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-10-31 23:14:19 +0530

    Committing files

---
+++ CodeChef/C++14/TEST/55381396.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+// The code is written by Tashvik Srivastava  // 
+#define GODSPEED ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define TEST int t; cin>>t; while(t--)
+#define loop1 for(int i =0;i<(sizeof(arr)/sizeof(arr[0]));i++)
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main()
+{
+  GODSPEED;
+ int i = 1;
+ while(i!=0)
+ {
+  int n;
+  cin>>n;
+  if(n==42)
+    break;
+  else
+    cout<<n<<endl;
+ }
+}
+ 
