3a8dcbad9394a4bfaea605075e2f7cdf037508ad
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-10-31 23:11:18 +0530

    Committing files

---
+++ CodeChef/C++17/PINBS/56017451.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include<stdlib.h>
+#include<sstream>
+using namespace std;
+// The code is written by Tashvik Srivastava  // 
+#define GODSPEED ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define TEST int t; cin>>t; while(t--)
+#define loop for(int i =0;i<(sizeof(arr)/sizeof(arr[0]));i++)
+#define ll long long
+#define ld long double
+#define all(a) (a).begin(), (a).end()
+#define sz(x) ((int)x.size())
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int dtob(string s);
+int prime(int n);
+int strtoi(string s);
+
+
+int main()
+{
+GODSPEED
+TEST
+{
+    string s;
+    bool flag = false;
+    cin>>s;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if(s[i]=='1' and (s[i+1]=='1' or s[i+1]=='0'))
+        {
+            flag = true;
+            cout << "Yes"<<endl;
+            break;
+        }
+    }
+    if(flag == false) cout << "No"<<endl;
+
+}
+
+return 0;
+}
