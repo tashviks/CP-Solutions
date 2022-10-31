97ed407c957799898036f1e5dd95f94d9de95d70
Tashvik Srivastava <105586514+tashviks@users.noreply.github.com>
2022-11-01 00:14:00 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1694B | Paranoid String/161874114.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h> 
+using namespace std;
+// The code is written by Tashvik Srivastava  // 
+#define GODSPEED ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+#define TEST int t; cin>>t; while(t--)
+#define NoTEST int t = 1; while(t--)
+#define inp(v) for (int i = 0; i < n; ++i) {cin>>temp;v.push_back(temp);}
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define pb push_back
+#define popb pop_back
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+ 
+//first try (time taken about 15 min)
+//best case complexity of O(n)
+ 
+void solve()
+{
+ int n ;
+ cin>>n;
+ string str;
+ cin>>str;
+ 
+ ll cnt = n;
+ 
+ for(int i = 1;i<n;i++)
+  if(str[i-1]!=str[i])
+   cnt = cnt + i;
+ 
+ 
+  cout<<cnt<<endl;
+ 
+    
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+int main() 
+{
+ GODSPEED
+ 
+    TEST
+    {
+        solve();
+    }
+}
+ 
